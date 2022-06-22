7d715a3625214ef7dbf7ba261fc8a7819ed9f53f
u2009n <u2009n@yahoo.com>
2022-06-22 16:51:57 +0200

    Add files via upload

--- pimimporter.php
+++ pimimporter.php
@@ -1,63 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./sweetalert/sweetalert2.all.min.js"></script>
+<    <script src="./sweetalert/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    
+
+
+
+
 <?php
 
 
    require_once('db_connection.php'); // call db connection to Datenbank
-
-        $mainFieldsName=null;
-        $preisFiledsName=null;
-       
-  
-   
+   $url="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; // Get the Url
 
 
-                        // To get the columns name from table map_ex_out and arrange them 
+//******************************************************************************************************************************//
+                function get_fields($result,$validZgFilelds){       // To get the columns name from table map_ex_out and arrange them 
 
-                function get_fields($sql,$conn){    
 
-                    
                               $index=0;
-                                $result = mysqli_query($conn,$sql);
-                                    if(mysqli_num_rows($result) > 0){
-                                      while($row = mysqli_fetch_array($result)){
-                                                if($index > 0){
-                                                  $fields[]= ($row['Field']);
-                                                }  
-                                              $index+=1;    
-                                        }
-                                        
-                                    }  
+                              if($validZgFilelds<>true){  
+                                  while($row = mysqli_fetch_array($result)){  //returen the main fields
+                                        if($index > 0){
+                                            $fields[]= ($row['Field']);
+                                          }  
+                                        $index+=1;    
+                                    }
+                                }else{
+                                 while($row = mysqli_fetch_array($result)){  //returen the zg fields
+                                        $fields[]= ($row['Field']);
 
-                      return $fields;          
+                                     }    
                               
-                
+                                } 
+                              
+                                return $fields;       
 
                 }
 
 
-                // To remove prifex zgi
+//******************************************************************************************************************************//
+                function get_Filter_zg($_fields,$tb_index){    // To remove prifex zgi and replace it with m_
 
-                function get_Filter_zg($_fields,$tb_index){
-
-
-                $len=strlen('zg'.$tb_index.'_'); 
-                        foreach($_fields as $field){
-                            $fields[]=is_numeric(substr($field,$len)) ? 'm_'.substr($field,$len) : substr($field,$len);
-                                
-                        }
-                        return $fields;
+              
+                  $len=strlen('zg'.$tb_index.'_'); 
+                          foreach($_fields as $field){
+                              $fields[]=is_numeric(substr($field,$len)) ? 'm_'.substr($field,$len) : substr($field,$len);
+                          }
+                          return $fields;
 
                 }
 
-                // Get qeuray statement
 
-                  function get_qeuray($fields){
+//******************************************************************************************************************************//
+                  function get_qeuray($fields){   // Arrange the Fields in order to Create new Table 
 
-                      $_fields=null;$index=0;
+                      $_fields=null;$index=0;$comma =", ";
+
                         for($index=0;$index < count($fields);$index++){
                             if($index==count($fields)-1){
-                                $_fields.=$fields[$index] .'  text ';
+                                $_fields.=$fields[$index] .'  text ';  //Arrange the fields with thier data-type 
                             }else{
-                              $_fields.=$fields[$index] .'  text ,';
+                              $_fields.=$fields[$index] .'  text '.$comma; // eadd commas to feild list
                             } 
                         }     
 
@@ -66,23 +76,17 @@
                   }
 
 
-                  // To DROP old tables 
-
-
-                function drop_table($conn,$tb_index){   
+//******************************************************************************************************************************//
+                function drop_table($conn,$tb_index){    // To DROP old tables 
                         $sql = "DROP TABLE IF EXISTS map_ex_out_zg".$tb_index."";
                         $conn->query($sql);
 
                 }
-
                   
-                // To Create Newc tables 
 
-                function Create_Tables($fields,$tb_index,$conn){   
-                 
-                 
-                  $path = "localhost/aladdin/public/act=pimimporter&I=zg".$tb_index;
-                 
+//******************************************************************************************************************************//
+                function Create_Tables($fields,$tb_index,$conn){    // To Create Newc tables 
+
                     $_fields=null;
                     $_fields=get_qeuray($fields);
                     drop_table($conn,$tb_index);
@@ -91,15 +95,12 @@
                           id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,".  $_fields."
                           )";
                         $conn->query($sql);
-                        if (!is_dir($path)) {
-                          mkdir($path, 0777, true);
-                      }
-
                     
                 }
 
 
-                function get_implode($fields){
+  //******************************************************************************************************************************//              
+                function get_implode($fields){     // get implode the fields from Array to query statmment
 
                   $qeuray=null;  $index=0;  $comma=',';
                     foreach($fields as $field){
@@ -113,24 +114,21 @@
                   
                 return $qeuray;
                 }
-                
 
-                    
-                    //Insert Data in New Tables
 
-                function Insert_data($alt_fields,$neu_fields,$tb_index,$conn){    
+    //******************************************************************************************************************************//         
+             
+    function Insert_data($alt_fields,$neu_fields,$tb_index,$conn){      //Insert Data in New Tables
                 
                     $table='map_ex_out';
-                    $new_table='map_ex_out_zg';
+                    $neu_table='map_ex_out_zg';
                     $sql_insert='';
 
 
-                    $alt_tb_fields=get_implode($alt_fields);
-                    $neu_tb_fields=get_implode($neu_fields);
+                   $alt_tb_fields=get_implode($alt_fields);//implode the fields with comma
+                    $neu_tb_fields=get_implode($neu_fields); //implode the fields with comma
                   
-                  
-
-                          $qeuray="SELECT   $alt_tb_fields  FROM $table";
+                          $qeuray="SELECT   $alt_tb_fields  FROM $table";  //Select the Data from Old Table map_ex_out
                           $result = mysqli_query($conn,$qeuray);
 
                             $count = count($alt_fields); $comma =", ";
@@ -156,60 +154,103 @@
                                                                   
                                 
                                     }
-                                    $sql_insert="INSERT INTO $new_table$tb_index ($neu_tb_fields) VALUES $values";
+                                    $sql_insert="INSERT INTO $neu_table$tb_index ($neu_tb_fields) VALUES $values"; //Insert Data in New Table
                                     $conn->query($sql_insert);
 
-                              
 
                             }  
-                      
                   
 
                 }
 
 
+    //*******************************************************    Main Code   **************************************************************************//
+    
+    $mainFieldsName=null;$preisFiledsName=null;
+    $tb_index=0; $max=0; $index=0; $_validZgFields=false;
+
+           if (isset($_GET['i'])) {
+                    
+                        if(is_numeric(substr($_GET['i'],2))){
+
+                              $index=substr($_GET['i'],2); $max=substr($_GET['i'],2);  // To get the Tablenummer when used als  indvidu
+
+                        }else{ 
+                            // echo '<script>alert("die Adresse oder die Tablenummer stimmt nicht")</script>';
+                              ?>
+
+                            <script>
+
+                              Swal.fire({
+                                icon: 'error',
+                                title: 'Warnung...',
+                                text: 'die Adresse oder die Tablenummer stimmt nicht',
+                              })
+                            </script>
+                                            
+                  <?php }
+      
+            }else{
+
+              $index=1; $max=5;
+            }    
+
+                   if($index<>0 && $max<>0){
+                         for ($tb_index=$index;$tb_index<=$max;$tb_index++ ){ 
+
+                            
+                              $mainFieldsName="SHOW columns from map_ex_out where field not like 'zg%'";  // Select the fields names that have no profex zg
+                              $preisFiledsName='SHOW columns from map_ex_out where field like';           // Select the fields names that have zg prifex
+                              $preisFiledsName.=" 'zg".$tb_index."%'";
+
+
+                                    $result = mysqli_query($conn,$mainFieldsName); // To get Main fields 
+                                    $fields=get_fields($result,'');
+
+                                    
+                                        $result = mysqli_query($conn,$preisFiledsName);   // To get ZG fields
+                                        if(mysqli_num_rows($result) > 0){  
+
+                                                    $_validZgFields=true;
+                                                      $prisFields=get_fields($result,$_validZgFields);
+                                                        $zg_fields=get_Filter_zg($prisFields,$tb_index);
+
+
+                                                           $_fields= array_merge($fields,$zg_fields);
+                                                              Create_Tables($_fields,$tb_index,$conn); 
+
+
+                                                           $alt_tb_fields= array_merge($fields,$prisFields);
+                                                        Insert_data($alt_tb_fields,$_fields,$tb_index,$conn);
+
+                                                      if (!is_dir('act=pimimporter&l=zg'.$tb_index)) {  // To create new director
+                                                    mkdir('act=pimimporter&l=zg'.$tb_index, 0777, true);
+                                                 }
+
+
+                                           }else{
+                                          echo '<script>alert("Es gibt keine Zielgruppe mit dieser Nummer")</script>';
+                                        }  
+
+                          }  
+                         if($_validZgFields !=false) 
+
+                       echo '<script>alert("die Tablle oder  Tabellen wurden erfolgreich erstellt")</script>';
+
+                   }    
+   
+    //*******************************************************    End Main Code   **************************************************************************//
+    ?>
 
 
 
 
-
-
-
-                //*************** Main Code *************************************/
-
-       for ($tb_index=1;$tb_index<=5;$tb_index++ ){ 
-
-                
-                  $mainFieldsName="SHOW columns from map_ex_out where field not like 'zg%'"; 
-                  $preisFiledsName='SHOW columns from map_ex_out where field like';
-                  $preisFiledsName.=" 'zg".$tb_index."%'";
-
-
-                        $fields=get_fields($mainFieldsName,$conn);
-                        $prisFields=get_fields($preisFiledsName,$conn);
-                        $zg_fields=get_Filter_zg($prisFields,$tb_index);
-
-
-                        $_fields= array_merge($fields,$zg_fields);
-                        Create_Tables($_fields,$tb_index,$conn); 
-
-
-                        $alt_tb_fields= array_merge($fields,$prisFields);  
-                        Insert_data($alt_tb_fields,$_fields,$tb_index,$conn);
-                   
-       }                 
-         echo '<script>alert("die Tabellen wurden erfolgreich erstellt")</script>';
-
-            CloseCon($conn);  
-?>
-
-
-
-
+<!-----------------------------------------------------------    HTML  Part     ------------------------------------------------------------------------------>
 
 <link rel='stylesheet' type='text/css' href='style.css'>    <!--Css file -->
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   <!--jQuray  file -->
+
+
 
 
 
@@ -218,10 +259,16 @@
      <div class="content">
          
      <form  method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-
-         <input type="hidden" name ="i" disabled>
+         <input type="hidden" name="i"> 
+         <input type="hidden" name ="pop" value="die Tabellen wurden erfolgreich erstellt" id="pop" >
          <input type="hidden" name="act" value="pimimporter" disabled>
          </form>   
      </div>   
      
-   
+</div>
+
+
+
+		
+
+
